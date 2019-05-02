cba48af31ee2a21c40ef9f0ae7c4e8e2d4f8b787
huberyoung <Huberyoung@163.com>
2019-05-02 08:33:54 +0000

    创建会话

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+
+class SessionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email'    => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            session()->flash('success','欢迎回来！');
+            return Redirect()->route('users.show',[Auth::user()]);
+        } else {
+            session()->flash('danger','很抱歉，您的邮箱和密码不匹配');
+            return Redirect()->back()->withInput();
+        }
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -2,8 +2,11 @@
   <div class="container">
     <a class="navbar-brand" href="{{route('home')}}">Weibo App</a>
     <ul class="navbar-nav justify-content-end">
+      @if (Auth::check())
       <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</a></li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        @else
+      @endif
     </ul>
   </div>
 </nav>
