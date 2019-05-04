6f3dbcb65e3cbe9651724808037ac8a59d6f6dbb
huberyoung <Huberyoung@163.com>
2019-05-04 04:10:11 +0000

    查看用户列表

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -22,7 +22,7 @@ class SessionsController extends Controller
      */
     public function index()
     {
-        //
+
     }
 
     /**
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user  =factory(User::class)->times(50)->make();
+        User::insert($user->makeVisible(['password','remember_token'])->toArray());
+        $user =User::find(1);
+        $user->name = 'Huberyoung';
+        $user->email = 'Huberyoung@163.com';
+        $user->save();
+    }
+}
